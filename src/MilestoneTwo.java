03abbafb02683a35df60033a4ec6dbc7b8b92413
James Ray <james@rayprogramming.com>
2017-12-11 22:47:02 -0500

    Added MileStoneTwo Test class

---
+++ src/MilestoneTwo.java
@@ -0,0 +1,21 @@
+/**
+ * @project Milestone2
+ * @course CST-135
+ * @author James Ray
+ * @author Christopher Hyde
+ * @author Jesus Leon
+ * @author Robert Wayde
+ * @teacher Dr. Lively
+ * @date 12/11/17
+ */
+
+public class MilestoneTwo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Todo: Test class
+    }
+    
+}
