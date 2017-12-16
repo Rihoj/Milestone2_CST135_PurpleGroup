fd830746f181cf8e420c5fa35ce5dc2dc4bb3152
James Ray <james@rayprogramming.com>
2017-12-16 14:36:36 -0500

    Update drink class based on UML. Add comments

--- src/vendingMachine/classes/Drink.java
+++ src/vendingMachine/classes/Drink.java
@@ -15,42 +15,164 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Drink extends Product {
 
+    /**
+     * Ounces in a drink.
+     */
     private double ounces;
+
+    /**
+     * Type of drink.
+     */
     private String type;
 
+    /**
+     * number of calories in the drink
+     */
+    private int calories;
+
+    /**
+     * sugar free status of the drink.
+     */
+    private boolean sugarFree;
+
+    /**
+     * amount of caffeine in the drink.
+     */
+    private int caffeine;
+
+    /**
+     * Create a drink with generic information.
+     */
     public Drink() {
         super();
         double randomOunces = ThreadLocalRandom.current().nextDouble(1, 11);
         DecimalFormat dec = new DecimalFormat("#.00");
         ounces = Double.parseDouble(dec.format(randomOunces));
         type = "Water";
+        sugarFree = ThreadLocalRandom.current().nextBoolean();
+        calories = ThreadLocalRandom.current().nextInt(0, 101);
     }
 
-    public Drink(double ounces, String type, String name, double price, int quantity) {
-        super(name, price, quantity);
+    /**
+     * Create a drink with specific information.
+     *
+     * @param ounces
+     * @param type
+     * @param calories
+     * @param sugarFree
+     * @param caffeine
+     * @param productName
+     * @param price
+     * @param quantity
+     * @param dispenceLocation
+     */
+    public Drink(double ounces, String type, int calories, boolean sugarFree, int caffeine, String productName, double price, int quantity, String dispenceLocation) {
+        super(productName, price, quantity, dispenceLocation);
         this.ounces = ounces;
         this.type = type;
+        this.calories = calories;
+        this.sugarFree = sugarFree;
+        this.caffeine = caffeine;
     }
 
+    /**
+     * Return string of drink class.
+     *
+     * @return String
+     */
     @Override
     public String toString() {
-        return "Drink{" + "ounces=" + ounces + ", type=" + type + '}';
+        return super.toString() + "\nDrink{" + "ounces=" + ounces + ", type=" + type + '}';
     }
 
+    /**
+     * get the ounces of the drink
+     *
+     * @return double
+     */
     public double getOunces() {
         return ounces;
     }
 
+    /**
+     * set the ounces of a drink
+     *
+     * @param ounces
+     */
     public void setOunces(double ounces) {
         this.ounces = ounces;
     }
 
+    /**
+     * get the type of drink
+     *
+     * @return String
+     */
     public String getType() {
         return type;
     }
 
+    /**
+     * Set the type of drink
+     *
+     * @param type
+     */
     public void setType(String type) {
         this.type = type;
+    }
+
+    /**
+     * Get the calories for the drink.
+     *
+     * @return int
+     */
+    public int getCalories() {
+        return calories;
+    }
+
+    /**
+     * Set the calories for the drink.
+     *
+     * @param calories
+     */
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+
+    /**
+     * Return if the drink is sugar free.
+     *
+     * @return boolean
+     */
+    public boolean isSugarFree() {
+        return sugarFree;
+    }
+
+    /**
+     * set the sugar free status of the drink.
+     *
+     * @param sugarFree
+     */
+    public void setSugarFree(boolean sugarFree) {
+        this.sugarFree = sugarFree;
+    }
+
+    /**
+     * Get how much caffeine is in the drink.
+     *
+     * @return int
+     */
+    public int getCaffeine() {
+        return caffeine;
+    }
+
+    /**
+     * sets how much caffeine is in the drink.
+     *
+     * @param caffeine
+     */
+    public void setCaffeine(int caffeine) {
+        this.caffeine = caffeine;
     }
 
 }
