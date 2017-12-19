500c792207845be10858c1be1d95f56480e1f55e
TerraNovaWork <34553221+TerraNovaWork@users.noreply.github.com>
2017-12-19 09:49:13 -0700

    Create Comparable.java

---
+++ src/vendingMachine/classes/products/Comparable.java
@@ -0,0 +1,10 @@
+package vendingMachine.classes.products;
+
+public interface Comparable {
+
+        //Comparable interface
+        public static void compareTo(){
+        
+        }
+        
+}
